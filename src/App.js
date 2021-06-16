d04586cfaaa4aa5dbacd11e602fb1e72b66c7b40
zhpadvoiski <zh.padvoiski@gmail.com>
2021-06-16 16:16:43 -0500

    styles updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import logo from "./logo.svg";
+import Minesweeper from "react-minesweeper";
+import "react-minesweeper/lib/minesweeper.css";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Minesweeper
+        onWin={() => console.log("GAME WON")}
+        onLose={() => console.log("GAME LOST")}
+        bombChance={0.15} // 15% chance that a field will contain a bomb
+        width={10} // amount of fields horizontally
+        height={10} // amount of fields vertically
+      />
+    </div>
+  );
 }
 
 export default App;
